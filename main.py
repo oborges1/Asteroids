4c81985c2b55fba3ad41f7a3a19e60dbef0d36b8
oborges1 <osvaldoborges1407@gmail.com>
2026-02-03 03:14:05 -0600

    uptate to groups

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+
 
 
 if __name__ == "__main__":
